76521fba8dbcc024c247ae5a4f76141025222537
emon12700 <hasan15-5601@diu.edu.bd>
2024-03-16 13:28:05 +0600

    visited added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+
 const Country = ({ country }) => {
     console.log(country);
     const { name, capital, flags, continents, timezones, population, area, cca3 } = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
+
     return (
         <div style={{border: "2px solid purple",borderRadius: "15px",padding: "10px",margin: "10px",}}>
             <h3>Country Name: {name.common}</h3>
@@ -12,8 +21,10 @@ const Country = ({ country }) => {
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p>Code: {cca3}</p>
+            <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+            {visited ? 'I have visited.' : 'I want to visit.'}
         </div>
     );
 };
 
-export default Country; 
+export default Country;
